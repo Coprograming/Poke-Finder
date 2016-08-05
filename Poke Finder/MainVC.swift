63b6818ae0d9ce824a24d5f02f9d812cd29175e0
Kasey Schlaudt <KC@Kaseys-Mac-mini.local>
2016-08-05 13:52:19 -0600

    installed Firebase

---
+++ Poke Finder/MainVC.swift
@@ -0,0 +1,74 @@
+//
+//  MainVC.swift
+//  Poke Finder
+//
+//  Created by Kasey Schlaudt on 8/4/16.
+//  Copyright © 2016 coprograming.com. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        map.delegate = self
+        map.userTrackingMode = MKUserTrackingMode.follow
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            map.showsUserLocation = true
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate , 2000, 2000)
+        
+        map.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        return annotationView
+    }
+    
+    @IBAction func pokeBallPressed(_ sender: AnyObject) {
+        
+    }
+}
